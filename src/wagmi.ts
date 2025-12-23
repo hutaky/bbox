c4b386d2475b28687ed4cf8f8eca4ff1e0028adc
madaXc <47150624+hutaky@users.noreply.github.com>
2025-12-23 22:10:16 +0100

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,2 @@
 // src/wagmi.ts
-import { createConfig, http } from "wagmi";
-import { base } from "wagmi/chains";
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
-
-export const wagmiConfig = createConfig({
-  chains: [base],
-  connectors: [farcasterFrame()],
-  transports: {
-    [base.id]: http(),
-  },
-});
+export {};
